f6fd3ee6e5f2e695c7a7713102ede990745d94fa
sabovyan <abovyan.sargis90@gmail.com>
2023-11-27 00:39:10 +0400

    [spaces] fix next dev and prisma issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient({
-  log: ['query']
-});
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ['query']
+  });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
--- notes.md
+++ notes.md
@@ -1,6 +1,12 @@
 - import order for eslint
 https://dev.to/otamnitram/sorting-your-imports-correctly-in-react-213m
 
-
--- pnpm issue with prisma
+- pnpm issue with prisma
 https://github.com/prisma/prisma/issues/2584#issuecomment-646414549
+
+- pnpm issue with auth types
+https://github.com/nextauthjs/next-auth/issues/6640#issuecomment-1441383198
+
+
+- prisma client issue with next dev server
+https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
