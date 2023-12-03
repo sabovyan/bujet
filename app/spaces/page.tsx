45b40338a512e4e076338daf8b2956f99f1a727d
sabovyan <abovyan.sargis90@gmail.com>
2023-12-03 16:19:56 +0400

    [spaces] rearrange spaces

--- app/spaces/page.tsx
+++ app/spaces/page.tsx
@@ -1,38 +1,43 @@
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 import { auth } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 
-export default async function Groups() {
+export default async function Spaces() {
   const session = await auth();
 
-  const email = session?.user?.email;
+  const id = session?.user?.id;
 
-  if (!email) {
+  if (!id) {
     redirect('/');
   }
 
-  const user = await prisma.user.findUnique({
+  const spaces = await prisma.space.findMany({
     where: {
-      email
-    },
-    include: {
-      spaces: true
+      creator: {
+        id
+      }
     }
   });
 
-  if (!user) {
-    // TODO handle it - either redirect it to sign it page or show not sign in view
-    throw new Error('handle it');
-  }
-
   return (
-    <main>
-      <header>
-        <h2>Your Space Management</h2>
-      </header>
-      {user.spaces.length ? (
-        <div>your spaces</div>
+    <main className="flex">
+      {spaces.length ? (
+        <aside>
+          <ul className="flex gap-2">
+            {spaces.map((sp) => (
+              <li
+                key={sp.id}
+                className="text-xl text-center min-w-[200px] p-2 rounded-lg border"
+              >
+                <Link href={`/spaces/${sp.id}`} className="block">
+                  {sp.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </aside>
       ) : (
         <div>no space was found</div>
       )}
