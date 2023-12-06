50ca0534814c4e63458a2a4ef472800430481188
sabovyan <abovyan.sargis90@gmail.com>
2023-12-06 11:46:01 +0400

    [spaces] add list not found page

--- app/spaces/[spaceId]/lists/[listId]/page.tsx
+++ app/spaces/[spaceId]/lists/[listId]/page.tsx
@@ -1,10 +1,21 @@
+import { notFound } from 'next/navigation';
+
+import { prisma } from '@/lib/prisma';
+
 import { SpaceItemParams } from '../../page';
 
 type ListItemParams = SpaceItemParams & {
   listId: string;
 };
-export default function ListItem({ params }: { params: ListItemParams }) {
-  console.log(params);
+export default async function List({ params }: { params: ListItemParams }) {
+  const list = await prisma.list.findUnique({
+    where: { id: params.listId }
+  });
+
+  if (!list) {
+    notFound();
+  }
+
   return (
     <div>
       <h1>{params.spaceId}</h1>
