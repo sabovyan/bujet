6eed9ea2c1b90f542ca44896e8ed12f2b859117d
sabovyan <abovyan.sargis90@gmail.com>
2023-12-06 10:53:38 +0400

    [spaces] remove unused var

--- app/spaces/add/page.tsx
+++ app/spaces/add/page.tsx
@@ -1,13 +1,11 @@
 import { addSpace } from '@/app/actions/space.actions';
 import { FormField } from '@/components/formField';
 import { Button } from '@/components/ui/button';
-import { auth } from '@/lib/auth';
 
 export default async function NewSpace() {
-  const session = await auth();
   return (
     <div>
-      <h1>ADD NEW SPACE</h1>
+      <h2>ADD NEW SPACE</h2>
 
       <form action={addSpace}>
         <FormField type="text" label="Name" name="name" />
