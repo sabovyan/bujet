2ac8859e491fa13efba501b6f292decc0b31946e
sabovyan <abovyan.sargis90@gmail.com>
2023-12-09 13:15:42 +0400

    [lists] add form for list items

--- app/spaces/[spaceId]/lists/[listId]/page.tsx
+++ app/spaces/[spaceId]/lists/[listId]/page.tsx
@@ -1,6 +1,8 @@
 import { notFound } from 'next/navigation';
 
 import { FavoriteListButton } from '@/components/FavoriteListButton';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { prisma } from '@/lib/prisma';
 
 import { SpaceItemParams } from '../../page';
@@ -18,14 +20,20 @@ export default async function List({ params }: { params: ListItemParams }) {
   }
 
   return (
-    <main>
+    <main className="p-4">
       <header className="flex gap-4">
-        <h2 className="text-2xl">{list.name}</h2>
-        <FavoriteListButton
-          spaceId={params.spaceId}
-          listId={list.id}
-          isFave={list.favorite}
-        />
+        <div className="flex gap-4">
+          <h2 className="text-2xl">{list.name}</h2>
+          <FavoriteListButton
+            spaceId={params.spaceId}
+            listId={list.id}
+            isFave={list.favorite}
+          />
+        </div>
+        <form className="flex gap-4">
+          <Input type="text" name="name" />
+          <Button>Add</Button>
+        </form>
       </header>
     </main>
   );
